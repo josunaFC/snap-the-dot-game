842db80f17637458f98399b2fefbfc6d67d0a37f
josunaFC <38095767+josunaFC@users.noreply.github.com>
2020-07-14 10:37:13 -0700

    Full game with 60 second timer!

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    if (Sprite.get(LedSpriteProperty.X) == 2) {
+        basic.showIcon(IconNames.Yes)
+        game.addScore(1)
+    } else {
+        basic.showIcon(IconNames.No)
+        game.addScore(-1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(game.score())
+})
+let Sprite: game.LedSprite = null
+Sprite = game.createSprite(2, 2)
+game.startCountdown(60000)
 basic.forever(function () {
-	
+    Sprite.move(1)
+    basic.pause(150)
+    Sprite.ifOnEdgeBounce()
 })
